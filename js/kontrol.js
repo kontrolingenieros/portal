e5b00f486ad9e69eb2723894c3e598fe54822d2d
kontrolingenieros <68480743+kontrolingenieros@users.noreply.github.com>
2020-08-08 12:55:18 -0500

    Add files via upload

---
+++ js/kontrol.js
@@ -0,0 +1,40 @@
+
+const designIco = document.getElementById('designIco'); 
+const equipmentIco = document.getElementById('equipmentIco'); 
+const implementationIco = document.getElementById('implementationIco'); 
+const maintenanceIco = document.getElementById('maintenanceIco'); 
+
+
+const designInfo = document.getElementById('designInfo');
+const equipmentInfo = document.getElementById('equipmentInfo');
+const instalationInfo = document.getElementById('implementationInfo');
+const maintenanceInfo = document.getElementById('maintenanceInfo');
+const sections = {
+    designInfo: designInfo,
+    equipmentInfo: equipmentInfo,
+    instalationInfo: instalationInfo,
+    maintenanceInfo: maintenanceInfo
+
+}
+
+const mostrarInfo = function(ev) {
+    hideInfo();
+
+    const section = ev.currentTarget.dataset.info;
+    console.log(section);    
+    const sec = sections[section].classList.remove('hide');
+};
+
+function hideInfo(){
+    
+    console.log('hide');
+    sections["designInfo"].classList.add('hide');
+    sections["equipmentInfo"].classList.add('hide');
+    sections["instalationInfo"].classList.add('hide');
+    sections["maintenanceInfo"].classList.add('hide');
+}
+
+designIco.addEventListener('click',mostrarInfo);
+equipmentIco.addEventListener('click',mostrarInfo);
+implementationIco.addEventListener('click',mostrarInfo);
+maintenanceIco.addEventListener('click',mostrarInfo);
